0d9287d958ad787d200f0c634896e99b3da2bb56
unknown <leodance1997@gmail.com>
2021-10-25 13:46:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Divider } from '@mui/material';
+//import { Divider } from '@mui/material';
 import React from 'react';
 import './App.css';
-//import Contact from './components/contact';
-import Content from './components/Content';
-import Cover from './components/Cover';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Contact from './components/contact';
+//import Content from './components/Content';
+//import Cover from './components/Cover';
+//import Footer from './components/Footer';
+//import Header from './components/Header';
 
 function App() {
   return (
     <div id="app" className="App">
-     <Header />  
+  
+    {/*   <Header />  
     <Cover />
     <Divider />
     <Content />
-    <Footer /> 
-    {/*<Contact />*/}
+    <Footer /> */}
+    <Contact />
     </div>
   );
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,31 +1,77 @@
-import {Container } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, Grid } from "@mui/material";
+//import { Box } from "@mui/system";
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./contact.css";
+import { Box } from "@mui/system";
 
 function Contact(){
+    
 
     return (
         <div id="contact">
             <Header />
-            <Container spacing={2} >
-                <Box >
+            {/*<Container >
+                <table>
                    <tr> <h1 className="con-head" >Contact Us</h1></tr> 
                     <tr> <p className="con-para">Let’s Book your Solar Power/Subsidized</p></tr>   
                       <tr>
-                         <td><input type="text" placeholder="your name?" className="name" /></td>
+                         <td>
+                             <label className="namel" >Name</label>
+                             <input type="text" placeholder="your name?" className="name" />
+                             </td>
                           </tr>  
-                        
+                          </table>   
                             
-                </Box>
 
                 <Box  className="contactbox" >
                     <image className="layer" />
                 </Box>
                 
-            </Container>
+            </Container>*/}
+            <Grid container fixed spacing={1} >
+                <Grid item xs={6}>
+                <h1 className="con-head" >Contact Us</h1>
+                <p className="con-para">Let’s Book your Solar Power/Subsidized</p>
+                <tr>
+                    <td>
+                        
+                    <input type="text"  className="name" />
+                    </td>
+                    <td>
+                    <input type="text"  className="mobile" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <input type="text"  className="email" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <h5 className="Add">Address</h5>
+                    <input type="text"  className="address" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <h5 className="source">Service Source</h5> 
+                    <input type="text"  className="service" />
+                    </td>
+                </tr>
+                <tr>
+                    <Button className="book" >Book Now</Button>
+                </tr>
+
+                </Grid>
+                <Grid item xs={6}>
+                <Box  className="contactbox" >
+                    <image className="layer" />
+                </Box>
+
+                </Grid>
+            </Grid>
             <Footer />
         </div>
     )
